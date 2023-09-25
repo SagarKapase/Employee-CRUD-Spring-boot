4372eb25c9b8889b487f880af3c61e40d153162c
SagarKapase <97930178+SagarKapase@users.noreply.github.com>
2023-09-25 17:15:03 +0530

    updated code

--- src/main/java/com/bootsecurity/SpringBootSecurity.java
+++ src/main/java/com/bootsecurity/SpringBootSecurity.java
@@ -1,4 +1,4 @@
-package com.fullstack;
+package com.bootsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/bootsecurity/SpringBootSecurityTests.java
+++ src/test/java/com/bootsecurity/SpringBootSecurityTests.java
@@ -1,4 +1,4 @@
-package com.fullstack;
+package com.bootsecurity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
